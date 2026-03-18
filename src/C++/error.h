26c5afcb94146f051b177f9a86142fc13375e882
Spencer <Spencerjw10@gmail.com>
2026-03-18 17:25:19 -0500

    Mar 18

---
+++ src/C++/error.h
@@ -0,0 +1,9 @@
+#ifndef TORTILLA_ERROR_H
+#define TORTILLA_ERROR_H
+
+
+class error {
+};
+
+
+#endif
--- src/C++/lexer.cpp
+++ src/C++/lexer.cpp
@@ -5,11 +5,17 @@
 using namespace std;
 
 class token {
-    public:
-        string kind; //Kinds: Variable Name, Keyword, Operator, Int, Float, Bool, Str
-        string val; //Value or specific type e.g. king = Operator, val = 'Plus'
-        int line;  //Line number
-        int col; //Column
+public:
+    string kind; //Kinds: Variable Name, Keyword, Operator, Int, Float, Bool, Str
+    string val; //Value or specific type e.g. king = Operator, val = 'Plus'
+    int line; //Line number
+    int col; //Column
+    token(string k, string v, int l, int c) {
+        kind = k;
+        val = v;
+        line = l;
+        col = c;
+    }
 };
 string getSingOp(char c) {
     switch(c) {
@@ -38,7 +44,51 @@ string getSingOp(char c) {
         default:  return "";   // not a single operator
     }
 }
-
+string getDoubOp(char first, char second) {
+    switch(first) {
+        case '+':
+            switch(second) {
+            case '+': return "Inc";
+            case '=': return "PlusEql";
+            default:  return "";
+            }
+        case '-':
+            switch(second) {
+            case '-': return "Dec";
+            case '=': return "MinusEql";
+            default:  return "";
+            }
+        case '*':
+            switch(second) {
+            case '*': return "Power";
+            case '=': return "TimesEql";
+            default:  return "";
+            }
+        case '/':
+            switch(second) {
+            case '=': return "DivideEql";
+            default:  return "";
+            }
+        case '%':
+            switch(second) {
+            case '=': return "ModEql";
+            default:  return "";
+            }
+        case '<':
+            switch(second) {
+            case '=': return "Lte";
+            case '<': return "Shl";
+            default:  return "";
+            }
+        case '>':
+            switch(second) {
+            case '=': return "Gte";
+            case '>': return "Shr";
+            default:  return "";
+            }
+        default: return "";
+    }
+}
 
 string code = "";
 int main() {
@@ -49,7 +99,7 @@ int main() {
     int line = 0;
     int col = 0;
     char qot = ' ';  //Quotation Type for Strings
-    auto value = ' ';
+    string value = " ";
     string abc = "asdfghjklqwertyuiopzxcvbnmASDFGHJKLQWERTYUIOPZXCVBNM_";
     string keywords[48] = {
         "is", "not", "in", "has", "xor", "nor", "and",
@@ -65,7 +115,41 @@ int main() {
     };
 
     while (i < code.length()) {
+        //Increment line and column
+        if (code[i] == '\n') {
+            line += 1;
+            col = 0;
+        }
+        else {
+            col += 1;
+        }
 
+        //Comment Removal
+        if (mode == 1 or mode == 2) {
+            if (i + 1 < code.length() and code[i] == '#' and code[i + 1] == '#') {
+                i += 1;
+                mode = 0;
+            }
+            else if (mode == 1 and code[i] == '\n') {
+                mode = 0;
+                i += 1;
+            }
+        }
+        //String Mode
+        else if (mode == 3) {
+            //Repeats until it finds the correct end quote
+            if (code[i] == qot) {
+                token curTok = token("Str", value, line, col);
+                tokens.push_back(curTok);
+                value = " ";
+                mode = 0;
+            }
+            else {
+                value += code[i];
+            }
+            i += 1;
+        }
+        mode += 1;
     }
     return 0;
 }
