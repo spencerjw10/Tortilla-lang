f3a1204fea4410631014a43bf39b206c63ab4444
spencerjw10 <spencerjw10@gmail.com>
2026-03-17 22:41:18 -0500

    Update Main.py

--- src/python/Main.py
+++ src/python/Main.py
@@ -2,12 +2,8 @@ from Lexer import tokenize
 import AST
 
 code = '''
-int jim = tom(10)
-forEach i in 77:
-end
-i += [1, 2, 3]
-int bob[0] -= timmothy.rad
+func john () null {tomm}
+bill{joe,}
 '''
 tokens = tokenize(code)
 tree = AST.Parse(AST.i, tokens, AST.states)
-print(tree.parsePrgm())
